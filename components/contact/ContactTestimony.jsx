2421e17307b95106f953363399c368bab9ae1d0a
Rémi Tafforeau <remitafforeau@gmail.com>
2025-08-01 10:57:05 +0200

    Update Contacttestimony

--- components/contact/ContactTestimony.jsx
+++ components/contact/ContactTestimony.jsx
@@ -6,21 +6,22 @@ export default function ContactTestimony() {
     <div className="space-y-4 prose">
       <h3 className="text-sm font-bold tracking-wide uppercase">ENEDIS</h3>
       <QuotedBlock className="">
-        Merci à Rémi pour son implication et son travail de qualité dans le développement de notre portail web. Son intervention a permis 
-        l’installation fluide de notre nouveau service.
+         Merci à Rémi pour son intervention sur l’IA et le développement web.
+        Plusieurs apprenants ont exprimé que ce moment avait été décisif dans leur choix de s’orienter vers le développements.
+        Une formation à fort impact, autant sur le fond que sur la motivation.
       </QuotedBlock>
 
       <div className="flex items-center gap-4 mt-4">
         <Image
-          src="/assets/images/avatar-bruno.png"
+          src="/assets/images/avatar-gilles-guenette.png"
           alt="Bruno G."
           width={40}
           height={40}
           className="rounded-full object-cover"
         />
         <div>
-          <p className="font-semibold">Bruno G.</p>
-          <p className="text-sm text-brand-fo">Chef de projet - ENEDIS</p>
+          <p className="font-semibold">Gilles G.</p>
+          <p className="text-sm text-brand-fo">Directeur Territorial Groupe OSENGO</p>
         </div>
       </div>
     </div>
